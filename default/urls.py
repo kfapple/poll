d4d16a67356f09c6709d24d3d64c52e6bdcc6103
kfapple <365goodlife@gmail.com>
2019-04-10 16:23:56 +0800

    2

--- default/urls.py
+++ default/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('poll/', poll_list),
-    path('poll1/', polllist.as_view()),
+    #path('poll/', poll_list),
+    path('poll/', polllist.as_view()),
+    path('poll/<int:pk>/',PollDetail.as_view())
 ]
--- default/views.py
+++ default/views.py
@@ -3,9 +3,12 @@ from django.views.generic import ListView, DetailView
 from .models import *
 
 # Create your views here.
-def poll_list(req):
-    polls= Poll.objects.all()
-    return render_to_response('poll_list.html', {'items':polls})
+#def poll_list(req):
+#    polls= Poll.objects.all()
+#    return render_to_response('poll_list.html', {'items':polls})
 
 class polllist(ListView):
+    model=Poll
+
+class PollDetail(DetailView):
     model=Poll
