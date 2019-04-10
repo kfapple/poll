96a4b266c0c476eea3a93efe6162d7d579c55f6f
kfapple <365goodlife@gmail.com>
2019-04-10 14:47:30 +0800

    1

--- default/views.py
+++ default/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.views.generic import ListView, DetailView
+from .models import *
 
 # Create your views here.
+def poll_list(req):
+    polls= Poll.objects.all()
+    return render_to_response('poll_list.html', {'items':polls})
+
+class polllist(ListView):
+    model=Poll
